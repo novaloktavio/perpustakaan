6ad25ea2f39ac0289bb5b7c4a7d792a0d628b6f7
novaloktavio <novaloktavio@gmail.com>
2017-03-05 15:35:51 +0700

    javascript

---
+++ js/myjs.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').slider();
+
+    $('.modal').modal();
+
+	$(".dropdown-button").dropdown();
+    
+    $('select').material_select();
+
+});
+        
